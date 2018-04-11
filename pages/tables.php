2cf5f2d648f05a4143294cfe128f513298a4f5c4
KarinGarciaZ <oscarin962011@hotmail.com>
2018-04-11 17:18:35 -0500

    Table of sales is finished.

--- pages/tables.php
+++ pages/tables.php
@@ -66,7 +66,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html"><img src="../assets/images/letras.png" alt="" style="width: 150px"></a>
+                <a class="navbar-brand" href="index.php"><img src="../assets/images/letras.png" alt="" style="width: 150px"></a>
             </div>
             <!-- /.navbar-header -->
 
@@ -95,10 +95,10 @@
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">                        
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Inicio</a>
+                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Inicio</a>
                         </li>                        
                         <li>
-                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Pedidos</a>
+                            <a href="tables.php"><i class="fa fa-table fa-fw"></i> Pedidos</a>
                         </li>     
                     </ul>
                 </div>
